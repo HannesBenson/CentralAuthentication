e37382ff7556800c3f7b69e378080db865eef9c3
Hannes Benson <hannes@mpowered.co.za>
2011-09-03 14:26:56 +0200

    change the connection file to use Rails.env variable to connect to available databases

--- lib/central_authentication.rb
+++ lib/central_authentication.rb
@@ -1,8 +1,7 @@
+require "authlogic"
+
 require "central_authentication/version"
+require "central_authentication/connection"
 require "central_authentication/migration"
 require "central_authentication/user"
 require "central_authentication/acts_as_central_authentication_user"
-require "central_authentication/connection"
-
-module CentralAuthentication
-end
