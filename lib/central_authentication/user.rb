e63657b29998f35bfba62e9ebc46fb885536fc29
Hannes Benson <hannes@mpowered.co.za>
2011-09-05 05:49:30 +0200

    Remove the transition_from_restful_authentication config option asit was duplicated and not always used

--- lib/central_authentication/user.rb
+++ lib/central_authentication/user.rb
@@ -1,6 +1,5 @@
 class CentralAuthentication::User < CentralAuthentication::Connection
   acts_as_authentic do |config|
-    config.transition_from_restful_authentication true
     config.merge_validates_length_of_password_field_options({:minimum => 9})
     config.merge_validates_length_of_password_confirmation_field_options({:minimum => 9})
     config.login_field = :email
