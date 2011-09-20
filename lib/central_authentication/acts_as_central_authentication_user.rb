c04f87e06c13dec5aac38eb636668158dd07247b
Hannes Benson <hannes@mpowered.co.za>
2011-09-20 09:13:11 +0200

    update all the methods with 'error' handling for when a cauth user does not exist. create methods to create and update the cauth user as required' '

--- lib/central_authentication/acts_as_central_authentication_user.rb
+++ lib/central_authentication/acts_as_central_authentication_user.rb
@@ -12,45 +12,68 @@ module ActiveRecord
 
           attr_accessor :password_confirmation, :password
 
+          #Here we set the authlogic options:
+          # 1. Email will be used as the login field
+          # 2. It doesn't validate the password field against a database field
+          # 3. It doesn't validate the login field. Authlogic automatically validates the email field.
           acts_as_authentic do |config|
             config.login_field = :email
             config.validate_password_field = false
+            config.validate_login_field = false
           end
 
           validates_presence_of :password
           validates_presence_of :password_confirmation
           validates_format_of :password, :with => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{9,15}$/,
                               :message => 'must contain at least one uppercase and one lowercase letter as well as at least one numerical character.'
-          validates_format_of :password, :with => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{9,15}$/,
+          validates_format_of :password_confirmation, :with => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{9,15}$/,
                               :message => 'must contain at least one uppercase and one lowercase letter as well as at least one numerical character.'
           validates_confirmation_of :password
 
-          before_create :create_or_set_cauth_user
+          before_save :create_or_set_cauth_user
         end
       end
       module InstanceMethods
-        def create_or_set_cauth_user
-          user = CentralAuthentication::User.find_by_email(self.email)
-          if user.nil?
-            user = CentralAuthentication::User.create(:email => self.email, :password => self.password, :password_confirmation => self.password_confirmation, :password_expires_on => Date.today + 30.days, :persistence_token => self.persistence_token)
+        def create_cauth_user
+            CentralAuthentication::User.create(:email => self.email, :password => self.password, :password_confirmation => self.password_confirmation, :password_expires_on => Date.today + 30.days, :persistence_token => self.persistence_token)
+        end
+
+        def update_cauth_user(cauth_user)
+          if self.password.blank?
+            cauth_user.update_attributes(:email => self.email)
+          else
+            cauth_user.update_attributes(:password => self.password, :password_confirmation => self.password_confirmation, :email => self.email)
           end
-          self.central_auth_user_id = user.id
+        end
+
+        def create_or_set_cauth_user
+          cauth_user = CentralAuthentication::User.find_by_email(self.email)
+          if cauth_user.nil?
+            cauth_user = create_cauth_user
+            self.central_auth_user_id = cauth_user.id
+          else
+            update_cauth_user(cauth_user)
+          end
         end
 
         def persistence_token
-          central_auth_user.persistence_token
+          # This method returns the persistence_token from the central authentication user. If this user does not exist yet it returns a arbitrary string otherwise it fails validation.
+          return 'a' if central_authentication_user.nil?
+          central_authentication_user.persistence_token
         end
 
         def persistence_token=(value)
-          central_auth_user.update_attribute(persistence_token, value)
+          return value if central_authentication_user.nil?
+          central_authentication_user.update_attribute(persistence_token, value)
         end
 
         def persistence_token_changed?
-          central_auth_user.persistence_token_changed?
+          return false if central_authentication_user.nil?
+          central_authentication_user.persistence_token_changed?
         end
 
         protected
-        def valid_central_auth_credentials(password)
+        def valid_central_authentication_credentials(password)
           CentralAuthentication::User.valid?(self.email, password)
         end
       end
