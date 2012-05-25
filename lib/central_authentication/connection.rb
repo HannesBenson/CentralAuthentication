162026972f2a4c19eb0bce3c75f58a7ff37311d9
Hannes Benson <hannes@mpowered.co.za>
2012-05-25 10:30:28 +0200

    add the connection instance method as required by Rails 3.2.X, also add the table name prefix method to correct the schema migration table issue

--- lib/central_authentication/connection.rb
+++ lib/central_authentication/connection.rb
@@ -1,4 +1,7 @@
 class CentralAuthentication::Connection < ActiveRecord::Base 
   establish_connection "cauth_#{Rails.env}"
   self.abstract_class = true
+  def self.table_name_prefix
+    "cauth_#{Rails.env}"
+  end
 end
--- lib/central_authentication/migration.rb
+++ lib/central_authentication/migration.rb
@@ -2,4 +2,8 @@ class CentralAuthentication::Migration < ActiveRecord::Migration
   def self.connection 
     CentralAuthentication::Connection.connection 
   end 
+
+  def connection 
+    CentralAuthentication::Connection.connection 
+  end 
 end
